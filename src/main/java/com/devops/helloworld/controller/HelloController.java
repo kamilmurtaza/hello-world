de9622dbb22685893dbd57b99b635daedbc1d85d
Kamil Murtaza <kamil.murtaza@tkxel.io>
2025-09-11 19:52:07 +0500

    Api Changes

--- src/main/java/com/devops/helloworld/controller/HelloController.java
+++ src/main/java/com/devops/helloworld/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.devops.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1")
 public class HelloController {
 
+    @Value("${greeting.customer.name}")
+    private String name;
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World! 🚀";
+    }
+
+    @GetMapping("/greetings")
+    public String sayCustomHello() {
+        return String.join("", "Presenting you Configurable {", name, "} \uD83D\uDE02");
     }
 }
