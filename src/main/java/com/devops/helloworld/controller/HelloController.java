e3156a0f91b8afd250e02121b94ecd091e8194c9
Kamil Murtaza <kamil.murtaza@tkxel.io>
2025-09-11 14:51:13 +0500

    Update

--- src/main/java/com/devops/helloworld/controller/HelloController.java
+++ src/main/java/com/devops/helloworld/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.devops.helloworld;
+package com.devops.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
